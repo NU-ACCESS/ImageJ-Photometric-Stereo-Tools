27e0b64a4d350831fbfd4965e99fe63f222dcab5
NU ACCESS <30444107+NU-ACCESS@users.noreply.github.com>
2020-01-01 20:59:39 -0600

    Add files via upload

--- Photometric_Stereo_.py
+++ Photometric_Stereo_.py
@@ -1,27 +1,27 @@
-#This solves the photometric stereo problem via least squares method.
-#Requires Jama jar to be downloaded from http://math.nist.gov/javanumerics/jama/
-#Rename the current downloaded version as just "Jama.jar" and place into plugins folder
-#This script relies on yython but is really bits and pieces from the macro recorder
-#Improvements  to come in the next version
-
 from ij import IJ, ImagePlus
 from ij.plugin import ImageCalculator
 from ij.process import ImageProcessor
 from ij.process import FloatProcessor
-import sys
-sys.path.append('Jama.jar')
-from Jama import Matrix
+from org.ejml.simple import SimpleMatrix 
+from fiji.util.gui  import GenericDialogPlus
+
+#Input parameters
+gd = GenericDialogPlus("Photometric Stereo")  
+gd.addDirectoryOrFileField("Select lighting direction file","")
+gd.showDialog()  
+
+directory_w = gd.getNextString()
 
 imp2 = IJ.getImage()
 IJ.run("32-bit")
 imp2 = IJ.getImage()
 
 #path to lighting directions
-IJ.run("Text Image... ", "open=")
+IJ.run("Text Image... ", "open="+str(directory_w))
 imp = IJ.getImage()
 imp.setTitle("Lighting directions")
 
-# making a jython nested list for constructing lighting direction matrix
+# constructing lighting direction matrix
 m = imp.getProcessor().getPixels()
 m2 = [val for val in m]
 L = [m2[i:i+imp.width] for i in range(0, len(m2), imp.width)]
@@ -37,53 +37,27 @@ for i in range(1, n_slices+1):
   I.append(n2)
 
 # construct Jama matrices and calculate normals
-B = Matrix(L) 
-C = Matrix(I)
-kN = B.solve(C).getColumnPackedCopy()# calculate normals via least squares
+B = SimpleMatrix(L) 
+C = SimpleMatrix(I)
+kN = B.solve(C).getMatrix().data# calculate normals via least squares
+L = [kN[i:i+imp2.height*imp2.width] for i in range(0, len(kN), imp2.height*imp2.width)]
 
-#return matrices to images 
-ipFloat = FloatProcessor(3, imp2.height*imp2.width,  kN)
-impa = ImagePlus("kN", ipFloat)
-impa.show()
+S = ImagePlus("X", FloatProcessor(imp2.width,imp2.height,L[0])).show()
+U = ImagePlus("Y", FloatProcessor(imp2.width,imp2.height,L[1])).show()
+V = ImagePlus("Z", FloatProcessor(imp2.width,imp2.height,L[2])).show()
 
-#reshape as using montage and reslice commands (need a better way to do this) 
-IJ.run(impa,"Montage to Stack...", "images_per_row=1 images_per_column=%d border=0" % imp2.height)
-impb = IJ.getImage()
-IJ.run("Reslice [/]...", "output=1.000 start=Left avoid")
-impc = IJ.getImage()
-
-impa.close()
-impb.close()
-
-#calculating albedo
+IJ.run("Images to Stack", "name=Stack title=[] use")
+imp = IJ.getImage()
+imp.setTitle("Normal Vector Map")
 IJ.run("Z Project...", "projection=[Max Intensity]")
-impd = IJ.getImage()
-impd.setTitle("albedo")
+impt = IJ.getImage()
 
-#calculating normals
-N = ImageCalculator().run("Divide create 32-bit stack", impc, impd)
+N = ImageCalculator().run("Divide create 32-bit stack", imp, impt)
 N.show()
-#IJ.run("Rotate 90 Degrees Right")
-#IJ.run("Flip Horizontally", "stack")
 impf = IJ.getImage()
 impf.setTitle("32 bit Surface Normal Map")
+IJ.run("Make Composite", "display=Composite")
 
-#converting to 8 bit color image
-IJ.run("Duplicate...", "duplicate")
-IJ.run("8-bit")
-impe = IJ.getImage()
-impe.changes = False
-IJ.run("Stack to RGB")
-impg = IJ.getImage()
-impg.setTitle("8 bit Surface Normal Map")
-
-impc.close()
-impd.close()
-impe.close()
-
-
-
-
-
-
+imp.close()
+impt.close()
 
